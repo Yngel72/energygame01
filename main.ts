1cfb7ecc38030a98d3fc8c3e99ee8a1ec4881742
Yngel72 <94963449+Yngel72@users.noreply.github.com>
2022-01-31 10:40:14 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,65 @@
+info.onCountdownEnd(function () {
+    if (info.score() < 20) {
+        game.over(false)
+    } else {
+        game.over(true)
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeScoreBy(1)
+})
+let energy: Sprite = null
+tiles.setTilemap(tilemap`level1`)
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    . f f f . f f f f . f f f . 
+    f f f f f c c c c f f f f f 
+    f f f f b c c c c b f f f f 
+    f f f c 3 c c c c 3 c f f f 
+    . f 3 3 c c c c c c 3 3 f . 
+    . f c c c c 4 4 c c c c f . 
+    . f f c c 4 4 4 4 c c f f . 
+    . f f f b f 4 4 f b f f f . 
+    . f f 4 1 f d d f 1 4 f f . 
+    . . f f d d d d d d f f . . 
+    . . e f e 4 4 4 4 e f e . . 
+    . e 4 f b 3 3 3 3 b f 4 e . 
+    . 4 d f 3 3 3 3 3 3 c d 4 . 
+    . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+    . . . . f f f f f f . . . . 
+    . . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+if (Math.percentChance(20)) {
+    tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
+} else {
+    tiles.placeOnRandomTile(mySprite, sprites.castle.tileGrass1)
+}
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+for (let index = 0; index < 100; index++) {
+    energy = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . f f f f . . . . . 
+        . . . . . . f f 5 5 f . . . . . 
+        . . . . . . f 5 5 f f . . . . . 
+        . . . . . f f 5 f f . . . . . . 
+        . . . . f f 5 5 f f f . . . . . 
+        . . . . f 5 5 5 5 5 f . . . . . 
+        . . . . f f f f 5 5 f . . . . . 
+        . . . . . . f 5 5 f f . . . . . 
+        . . . . . f f 5 f f . . . . . . 
+        . . . . . f 5 f f . . . . . . . 
+        . . . . . f f f . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    if (Math.percentChance(80)) {
+        tiles.placeOnRandomTile(energy, sprites.castle.tilePath5)
+    } else {
+        tiles.placeOnRandomTile(energy, sprites.castle.tileGrass1)
+    }
+}
+info.startCountdown(10)
